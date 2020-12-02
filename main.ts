cf2c710b40d24e05da42ae540a9d74f5857341df
yoyo0281 <40514257+yoyo0281@users.noreply.github.com>
2020-12-02 23:20:56 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ let dist = 0
 basic.forever(function () {
     dist = sonar.ping(
     DigitalPin.P0,
-    DigitalPin.P0,
+    DigitalPin.P1,
     PingUnit.Centimeters
     )
     basic.showNumber(dist)
